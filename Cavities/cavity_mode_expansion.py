055c0b6364de04de84f882498462e8f4c9b6d391
StephenJamesCashen <43123083+StephenJamesCashen@users.noreply.github.com>
2021-03-23 00:38:38 -0700

    Band diagram up

--- Cavities/cavity_mode_expansion.py
+++ Cavities/cavity_mode_expansion.py
@@ -2,15 +2,11 @@ import numpy as np
 import legume
 
 
-
-
-
 class CavityModeExpansion:
 
-    def __init__(self, base_crystal, super_crystal, super_periods,
-                 polarization, defect_margins=(0.25, 0.25), layer=0, gmax=3, base_gmax=4):
+    def __init__(self, phc, base_phc, super_periods, defect_margins, layer=0, gmax=3, base_gmax=4):
 
-        self.base = base_crystal
+        self.base = base_phc
         self.layer = 0
         self.dslab = self.base.layers[layer].d
 
@@ -20,19 +16,15 @@ class CavityModeExpansion:
         self.marg_x = defect_margins[0]
 
         self.marg_y = defect_margins[1]
-        self.pole = polarization
 
-        self.freqs_im = None
-        self.rad_coup = None
-        self.rad_gvec = None
-        self.q_factor = None
-        self.band_gaps = None
         self.arg_list = []
-        self.gme = legume.GuidedModeExp(super_crystal, gmax=gmax)
-        self.base_gme = legume.GuidedModeExp(base_crystal, gmax=base_gmax)
+        self.gme = legume.GuidedModeExp(phc, gmax=gmax)
+        self.base_gme = legume.GuidedModeExp(base_phc, gmax=base_gmax)
 
     def mode_volume(self, gme, sample_scale, kind, mind):
-        # Get the electric field in the center of the slab
+        """
+        Get the Max
+        """
         s_nx = int(sample_scale * self.nx)
         s_ny = int(sample_scale * self.ny)
 
@@ -51,25 +43,6 @@ class CavityModeExpansion:
     def set_arglist(self, arg_list):
         self.arg_list = arg_list
 
-    def get_im_freq(self):
-        freqs_im = []
-        rad_coup = []
-        rad_gvec = []
-        for kind, minds in self.arg_list:
-            if self.gme.freqs_im.size() == 0:
-                freqs_im, rad_coup, rad_gvec = self.gme.compute_rad(kind=kind, minds=minds)
-                freqs_im.append([kind, freqs_im])
-                rad_coup.append([kind, rad_coup])
-                rad_gvec.append([kind, rad_gvec])
-            else:
-                freqs_im.append([kind, self.gme.freqs_im[kind][minds]])
-                rad_coup.append([kind, self.gme.rad_coup[kind][minds]])
-                rad_gvec.append([kind, self.gme.rad_gvec[kind][minds]])
-
-        self.freqs_im = np.array(freqs_im)
-        self.rad_coup = np.array(rad_coup)
-        self.rad_gvec = np.array(rad_gvec)
-
     def set_q_factor(self):
         if self.freqs_im is not None:
             q_factor = []
@@ -81,7 +54,7 @@ class CavityModeExpansion:
         else:
             raise ValueError("Imaginary frequencies have not yet been found.")
 
-    def find_band_gaps(self, sample_rate=10, order=np.array([0]), band_tol=0.1):
+    def find_band_gaps(self, sample_rate=10, order=np.array([0]), band_tol=0.1, lc_tol=0, numeig=20):
         lattice = self.base.lattice
 
         b1 = lattice.b1
@@ -91,21 +64,24 @@ class CavityModeExpansion:
                                 sample_rate * int(np.linalg.norm(b1 - b2) / np.pi),
                                 sample_rate * int(np.linalg.norm(b1) / np.pi)])
 
-        if self.base_gme.freqs.size() == 0:
+        if len(self.base_gme.freqs) == 0:
             self.base_gme.run(kpoints=path['kpoints'],
-                              gmode_inds=order + self.pole,
-                              numeig=20,
+                              gmode_inds=order,
+                              numeig=numeig,
                               verbose=False)
 
-        freqs = np.sort(self.base_gme.freqs.flatten())
-        gaps = np.argsort(np.diff(np.sort(self.base_gme.freqs.flatten())))
+        k_squared = np.tile((self.base_gme.kpoints[0]**2+self.base_gme.kpoints[1]**2)**(1/2), (numeig, 1)).T
+        in_lc_freqs = self.base_gme.freqs[self.base_gme.freqs/(k_squared+1e-12) <= 1/(2*np.pi) + lc_tol]
+
+        freqs_flat = np.sort(in_lc_freqs)
+
+        gaps = np.diff(freqs_flat)
         band_gaps = []
+        for i in range(gaps.size):
+            if gaps[i] >= band_tol:
+                band_gaps.append([freqs_flat[i], freqs_flat[i + 1], (freqs_flat[i] + freqs_flat[i + 1]) / 2])
 
-        i = 0
-        while gaps[i] >= band_tol:
-            band_gaps.append([freqs[gaps[i]], freqs[gaps[i]+1], (freqs[gaps[i]]+freqs[gaps[i]+1])/2])
+        gmg_ratio = np.array([(band_gap[1] - band_gap[0]) / band_gap[2] for band_gap in band_gaps])
+        band_gaps = np.array(band_gaps)
 
-        self.band_gaps = band_gaps
-
-        return band_gaps
-
+        return band_gaps, gmg_ratio
